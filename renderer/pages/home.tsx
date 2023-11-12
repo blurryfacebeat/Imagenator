5220fa746f2361351d9fcee9d1f9401cd4fd4987
blurryfacebeat <blurryfacebeat@gmail.com>
2023-11-12 14:48:57 +0700

    feat: Add ESLint

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function HomePage() {
-  const [message, setMessage] = React.useState('No message found')
+  const [message, setMessage] = React.useState('No message found');
 
   React.useEffect(() => {
     window.ipc.on('message', (message: string) => {
-      setMessage(message)
-    })
-  }, [])
+      setMessage(message);
+    });
+  }, []);
 
   return (
     <React.Fragment>
@@ -34,7 +34,7 @@ export default function HomePage() {
       <div>
         <button
           onClick={() => {
-            window.ipc.send('message', 'Hello')
+            window.ipc.send('message', 'Hello');
           }}
         >
           Test IPC
@@ -42,5 +42,5 @@ export default function HomePage() {
         <p>{message}</p>
       </div>
     </React.Fragment>
-  )
+  );
 }
